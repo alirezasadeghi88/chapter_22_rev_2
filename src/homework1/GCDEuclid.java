ebc907faca8cdcc392a17708e2dadb949b28edcf
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-02-26 05:32:54 -0800

    add GCDEuclid

---
+++ src/homework1/GCDEuclid.java
@@ -0,0 +1,24 @@
+package homework1;
+
+import java.util.Scanner;
+
+public class GCDEuclid {
+    public static int gcd(int m, int n) {
+        if (m % n == 0)
+            return n;
+        else
+            return gcd(n, m % n);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first integer: ");
+        int m = scanner.nextInt();
+        System.out.print("Enter second integer: ");
+        int n = scanner.nextInt();
+
+        System.out.println("The greatest common divisor for " + m +
+                " and " + n + " is " + gcd(m, n));
+    }
+}
